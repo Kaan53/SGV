51f7430c1d168c7270eb845a15795da1f038e7d4
Kaan53 <kaan.erdemm1@gmail.com>
2020-04-03 19:02:06 +0300

    siber güvenlik vize

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            XDocument xd = XDocument.Load("Sonuc.xml");
+            XName x = XName.Get("host", xd.Root.Name.NamespaceName);
+
+            foreach (XElement data in xd.Descendants(x))
+            {
+                Console.WriteLine(data.ToString());
+                xd.Save("Saved.xml");
+            }
+            Console.ReadKey();
+        }
+    }
+}
